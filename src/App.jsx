c217b9610737da34c58419f1634653c7df4e5a9b
inesmariao <63691309+inesmariao@users.noreply.github.com>
2024-09-17 20:08:11 -0500

    my first component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { MyFirstComponent } from './components/MyFirstComponent';
+
+function App() {
+
+  return (
+    <>
+      <h1>Fundamentos de React</h1>
+      <MyFirstComponent />
+    </>
+  )
+};
+
+export default App
---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,15 @@
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+      <div>MyFirstComponent</div>
+      <h1>Temas de React</h1>
+      <ul>
+          <li>Componentes</li>
+          <li>Eventos</li>
+          <li>Estados Hooks</li>
+          <li>Props</li>
+      </ul>
+    </>
+  )
+}
