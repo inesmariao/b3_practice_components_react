7e8546b92b2314614dfb407b5d726808bfbca23e
inesmariao <63691309+inesmariao@users.noreply.github.com>
2024-09-18 20:33:18 -0500

    ForEach and map examples

--- src/components/SecondComponent.jsx
+++ src/components/SecondComponent.jsx
@@ -13,6 +13,11 @@ export const SecondComponent = () => {
     "Otro libro"
   ];
 
+  const listBooks = [];
+  books.forEach((book, index) => {
+    listBooks.push(<li key={ index }> {book} </li>);
+  });
+
   return (
     <>
       <h1>SecondComponent</h1>
@@ -23,6 +28,9 @@ export const SecondComponent = () => {
               return <li key={ index }> { book } </li>
             })
           }
+        </ul>
+        <ul>
+          { listBooks }
         </ul>
       </div>
     </>
